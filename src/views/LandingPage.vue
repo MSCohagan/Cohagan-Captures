4040d0815d6b715f8451416c4f3d5473ac54d8a2
Michael Cohagan <mic@michaels-mini.mynetworksettings.com>
2024-08-22 22:30:23 -0600

    Ensure all necessary files are included

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <section class="section is-medium header">
+    <h1 class="is-size-2">Welcome to Cohagan Creates!</h1>
+  </section>
+  <section class="section featured-box">
+    <h2 class="is-size-3">Featured Games</h2>
+  </section>
+  <section class="section featured-box">
+    <b-carousel>
+      <b-carousel-item>
+        <h3>State Capital Guessing Game</h3>
+        <b-tooltip label="State Capital Guessing Game" position="is-top">
+          <div>
+            <a class="states-link" href="/statesGame">
+              <b-image src="src/assets/states.svg" class="states-svg" />
+            </a>
+          </div>
+        </b-tooltip>
+      </b-carousel-item>
+    </b-carousel>
+  </section>
+</template>
+<script setup lang="ts"></script>
+<style lang="css">
+.header {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+
+.featured-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+}
+
+.states-link {
+  display: flex;
+  justify-content: center;
+}
+
+.states-svg {
+  max-width: 50%;
+}
+</style>
