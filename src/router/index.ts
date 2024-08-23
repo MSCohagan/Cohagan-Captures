e07dbf653795f4672bfe5e184683d74e5a693c28
Michael Cohagan <mic@michaels-mini.mynetworksettings.com>
2024-08-22 21:50:33 -0600

    changing import location

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import StatesGame from '../views/StatesGame.vue';
-//import LandingPage from '../views/LandingPage.vue';
-import EuropeGame from '../views/EuropeGame.vue';
+import LandingPage from '../views/LandingPage.vue';
+import EuropeGame from '@/views/EuropeGame.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +10,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: StatesGame
+      component: LandingPage
     },
     {
       path: '/statesGame',
