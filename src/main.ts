2227b4a79f4368052837417244084ed6e0179fab
Michael Cohagan <mic@michaels-mini.mynetworksettings.com>
2024-08-22 21:28:54 -0600

    ts ignore on buefy

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import Buefy from 'buefy-next';
+import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import { createPinia } from 'pinia';
 
@@ -9,6 +9,7 @@ import router from './router';
 const app = createApp(App);
 
 app.use(createPinia());
+// @ts-ignore
 app.use(Buefy);
 app.use(router);
 
